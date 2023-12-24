ffa406a437fb495d51b0e39b762c715c36648093
Bhaskar Kaura <85757758+bhaskarblur@users.noreply.github.com>
2023-12-24 21:15:13 +0530

    changed repo name

--- app/src/main/java/com/bhaskarblur/gptbot/Repo/chatRepo.kt
+++ app/src/main/java/com/bhaskarblur/gptbot/Repo/chatRepo.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
-class chatGptRepo @Inject constructor(
+class chatRepo @Inject constructor(
     private var apiImpl: ApiImpl,
     private var realmDao: realmDao
 ) {
